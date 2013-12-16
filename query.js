57c6fda5a3ee2beabf49ca87d3e741c6a2068210
Clark Kampfe <clark.kampfe@gmail.com>
2013-12-15 19:43:00 -0600

    working JSONP

--- query.js
+++ query.js
@@ -1,35 +1,31 @@
-/* QUERY YQL */
-var results = "";
+var results; // global space for future access
 
-// callback
-function yahooHandler(data) {
+function clickHandler(targetUrl, tags, event){
+  event.preventDefault();
+  makeQuery(makeUrl(targetUrl, tags));
 }
 
-
-/* ON CLICK */
-function clickHandler(url, tags){
-  console.log("query()");
-
+function makeUrl(targetUrl, tags) {
   var baseURI          = "http://query.yahooapis.com/v1/public/yql?q=",
-      encodedTargetUrl = encodeURIComponent('http://') + url,
+      encodedTargetUrl = encodeURIComponent('http://') + targetUrl,
       encodedQuery     = encodeURI("select * from html where url")
-                     + encodeURIComponent("='") + encodedTargetUrl + "\'",
+                         + encodeURIComponent("='") + encodedTargetUrl + "\'",
       suffix           = "&format=json&diagnostics=true&callback=yahooHandler";
 
-
-  var complete = baseURI + encodedQuery + suffix;
-  console.log(complete);
+  return baseURI + encodedQuery + suffix;
 }
 
+function makeQuery(url) {
+  // make script element
+  var scriptEl  = document.createElement("script");
+  scriptEl.type = "text/javascript";
+  scriptEl.src  = url;
 
-// actual
-// http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fzeroclarkthirty.com'&format=json&diagnostics=true&callback=yahooHandler
-// http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fzeroclarkthirty.com'&format=json&diagnostics=true&callback=yahooHandler
-// http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fzeroclarkthirty.com'&format=json&diagnostics=true&callback=yahooHandler
+  // insert script element
+  document.body.appendChild(scriptEl);
+}
 
-
-
-
-
-
-
+// JSONP callback
+function yahooHandler(response) {
+  results = response.query.results;
+}
