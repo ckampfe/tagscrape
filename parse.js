dfc501a6fb3bebab1fd6ef8f3d3cdb8e7ed89a63
Clark Kampfe <clark.kampfe@gmail.com>
2013-12-19 15:41:47 -0600

    not sure, rewriting, commiting for posterity

--- parse.js
+++ parse.js
@@ -1,86 +1,87 @@
-/*** all extensions/helpers are defined at the bottom of their scope ***/
+/* extensions/helpers/private are defined at the bottom of their scope */
 
-// parse results, add to matches
+/* INIT */
 function parseResults(tags, results) {
-  var matches    = makeMatchesObj(tags),
-      parentNode = "body";
+  var matches    = (function() {
+                     var matches = {};
+                     tags.forEach(function(tag) {
+                       matches[tag] = [];
+                     });
+
+                     return matches;
+                   })();
+
+  var parentNode = "body";
+
   return findMatches(tags, matches, results.body, parentNode);
 }
 
+/* MAIN */
 function findMatches(tags, matches, node, parentNode) {
   var interests = ["class", "id", "content", "href", "span"];
 
-  // if object not array
+  // if object && not array
   if (typeof node === 'object' && !node.isArray) {
-    var keys = Object.keys(node); // children
+    var children = Object.keys(node); // children
 
-    if (tags.include(parentNode)) { // if parent is of interest, make ready!
-      var temp = {};
-      keys.forEach(function(key) {
-        if (interests.include(key)) { // if there's a match
-          temp[key] = node[key];
-        }
-      });
-
-      if (Object.keys(temp).length > 0) { // if any matches
-        matches[parentNode].push(temp);
-      }
+    // then, if parent is something we want
+    if (tags.include(parentNode)) {
+      collectMatches(matches);
     }
 
-    // descend if children have children
-    keys.forEach(function(key) {
+    // descend
+    children.forEach(function(key) {
       if (hasChildren(key)) {
         return findMatches(tags, matches, node[key], key);
       }
     });
 
-   } else if (parentNode.isArray) {
-     console.log("is array");
-  } else { // error
+  } else if (node.isArray) {
+    console.log("is array");
+  } else { // otherwise
     console.log("something fucked up");
   }
 
-  function hasChildren(key) {
-    if (node[key] !== null) {
-      if (node.isArray) {
-        iterateArray(node[key], key);
-      } else if (typeof node[key] === 'string') {
-        // eat it
-      } else if (Object.keys(node[key]).length) {
-        // console.log( node[key] + " has children");
-        return true;
-      } else {
-        // eat it
+  return matches;
+
+
+  /* PRIVATE / HELPERS */
+
+  // adding actual matches to matches obj
+  function collectMatches(matches) {
+    var temp = {};
+    children.forEach(function(key) {
+      if (interests.include(key)) { // if there's a match
+        temp[key] = node[key];
       }
+    });
+
+    if (Object.keys(temp).length > 0) { // if any matches
+      matches[parentNode].push(temp);
     }
   }
 
-  return matches;
-}
-
-/***
- * HELPERS
- ***/
-
-function iterateArray(arrayNode, parentNode) {
-  for (var i = 0; i < arrayNode.length; i++) {
-    return findMatches(tags, matches, node[i], parentNode);
+  // checking if a given key has children
+  function hasChildren(key) {
+    if (node[key] !== null) {
+      if (node[key].length > 1) {
+        // console.log("array");
+        for (var i = 0; i < node[key].length; i++) {
+          return findMatches(tags, matches, node[i], parentNode);
+        }
+      } else if (typeof node[key] === 'string') {
+        console.log("string");
+      } else if (Object.keys(node[key]).length) {
+        console.log( node[key] + " has children");
+        return true;
+      } else {
+        console.log("else");
+      }
+    }
   }
 }
 
-/* matches creator helper */
-function makeMatchesObj(tags) {
-  var matches = {};
-  tags.forEach(function(tag) {
-    matches[tag] = [];
-  });
-
-  return matches;
-}
-
-/* return true if Array includes
- * a given arg, else false
- **/
+// like Ruby's Array#include?
 Array.prototype.include = function(thing) {
   var lengthTarget = this.filter(function(arrayItem) {
     return arrayItem === thing;
